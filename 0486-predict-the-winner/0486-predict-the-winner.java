9123008aa2403156ba12dde52293dbb6ee406c02
Kunj Agarwal <kunjagarwal051@gmail.com>
2026-08-02 01:33:34 +0530

    Time: 1 ms (52.06%), Space: 42.9 MB (41.35%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+
+                int pickLeft = nums[i] - dp[i + 1][j];
+                int pickRight = nums[j] - dp[i][j - 1];
+
+                if (pickLeft > pickRight) {
+                    dp[i][j] = pickLeft;
+                } else {
+                    dp[i][j] = pickRight;
+                }
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+    }
+}
