7b1f6d13124b5c21a2b79b5334a45be8427ce531
Kunj Agarwal <kunjagarwal051@gmail.com>
2026-08-12 23:13:57 +0530

    Time: 2 ms (84.48%), Space: 47.5 MB (34.41%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int n=arr.length;
+        int left=0;
+        while(left+1<n && arr[left]<arr[left+1]){
+            left++;
+        }
+        if(left==0 || left==n-1){
+            return false;
+        }
+        while(left+1<n && arr[left]>arr[left+1]){
+            left++;
+        }
+        return left==n-1;
+    }
+}
