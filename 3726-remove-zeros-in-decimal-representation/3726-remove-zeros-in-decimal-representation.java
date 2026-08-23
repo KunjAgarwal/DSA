2478b6be8d8eea01dc0c065fe669999a2c6a270c
Kunj Agarwal <kunjagarwal051@gmail.com>
2026-08-23 23:52:01 +0530

    Time: 2 ms (53.42%), Space: 42.6 MB (68.59%) - LeetHub

--- 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.java
+++ 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.java
@@ -1,17 +1,12 @@
 class Solution {
     public long removeZeros(long n) {
-        String s=String.valueOf(n);
-        Stack<Character> st = new Stack<>();
-        for(char c:s.toCharArray()){
-            if(c!='0'){
-                st.push(c);
+        String num=Long.toString(n);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<num.length();i++){
+            if(num.charAt(i) !='0'){
+                sb.append(num.charAt(i));
             }
         }
-        StringBuilder ans = new StringBuilder();
-        while(!st.isEmpty()){
-            ans.append(st.pop());
-        }
-        ans.reverse();
-        return Long.parseLong(ans.toString());
+        return Long.parseLong(sb.toString());
     }
 }
