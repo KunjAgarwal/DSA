cb94c6629367910c5d008146f8d671150d68fe14
Kunj Agarwal <kunjagarwal051@gmail.com>
2026-09-14 06:58:50 +0530

    Time: 4 ms (84.57%), Space: 47.3 MB (12.19%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] arr = s.split(" ");
+        for(String i:arr){
+            sb.append(new StringBuilder(i).reverse());
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
