4781d3c4c99f1ca9fd8a7d18adb1f4bcccdb14f4
Kunj Agarwal <kunjagarwal051@gmail.com>
2026-09-20 20:21:01 +0530

    Time: 3 ms (85.93%), Space: 43.4 MB (40.99%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='(' || c=='[' || c=='{'){
+                stk.push(c);
+            }else{
+                if(stk.isEmpty()){
+                    return false;
+                }
+                char top=stk.pop();
+                if(c==')' && top!='('){
+                    return false;
+                }
+                if(c==']' && top!='['){
+                    return false;
+                }
+                if(c=='}' && top!='{'){
+                    return false;
+                }
+            }
+        }
+        return stk.isEmpty();
+    }
+}
